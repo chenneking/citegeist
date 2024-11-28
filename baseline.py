e257e586e9ddb28c769e3cf504c6ad05056ad67e
chenneking <39014772+chenneking@users.noreply.github.com>
2024-11-27 21:09:22 -0500

    Updated baseline

--- baseline.py
+++ baseline.py
@@ -2,11 +2,12 @@ from bertopic import BERTopic
 from sentence_transformers import SentenceTransformer
 from pymilvus import MilvusClient
 import json
-from helpers import load_api_key
+from helpers import load_api_key, generate_summary_prompt
 from azure_client import AzureClient
+from citations import get_arxiv_abstract
 
 AZURE_ENDPOINT = "cai-project"
-AZURE_PROMPTING_MODEL = "gpt-4o-mini"
+AZURE_PROMPTING_MODEL = "gpt-4o"
 AZURE_PROMPTING_MODEL_VERSION = "2024-08-01-preview"
 KEY_LOCATION = "carl_config.json"
 
@@ -18,6 +19,7 @@ prompting_client = AzureClient(
     deployment_id=AZURE_PROMPTING_MODEL,
     api_key=load_api_key(KEY_LOCATION)
 )
+
 
 abstract = 'The dominant sequence transduction models are based on complex recurrent or convolutional neural networks in an encoder-decoder configuration. The best performing models also connect the encoder and decoder through an attention mechanism. We propose a new simple network architecture, the Transformer, based solely on attention mechanisms, dispensing with recurrence and convolutions entirely. Experiments on two machine translation tasks show these models to be superior in quality while being more parallelizable and requiring significantly less time to train. Our model achieves 28.4 BLEU on the WMT 2014 English-to-German translation task, improving over the existing best results, including ensembles by over 2 BLEU. On the WMT 2014 English-to-French translation task, our model establishes a new single-model state-of-the-art BLEU score of 41.8 after training for 3.5 days on eight GPUs, a small fraction of the training costs of the best models from the literature. We show that the Transformer generalizes well to other tasks by applying it successfully to English constituency parsing both with large and limited training data.'
 embedded_abstract = embedding_model.encode(abstract)
@@ -38,7 +40,10 @@ res = client.search(
 result = json.dumps(res, indent=4)
 print(result)
 
-# prompt = 'Say: Hello World!'
-#
-# response: str = prompting_client.get_completions(prompt, AZURE_PROMPTING_MODEL_VERSION)
-# print(response)
+for obj in result[0]:
+    obj_arxiv_abstract = get_arxiv_abstract(obj['id'])
+    print(generate_summary_prompt(abstract, obj_arxiv_abstract))
+    # response: str = prompting_client.get_completions(generate_summary_prompt(abstract, obj_arxiv_abstract), AZURE_PROMPTING_MODEL_VERSION)
+    # print(response)
+
+

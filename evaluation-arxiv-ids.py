67d7622251b8e6a227794c0281125defad710213
chenneking <39014772+chenneking@users.noreply.github.com>
2024-12-15 23:26:08 -0500

    added related work comparison scoring

--- evaluation-arxiv-ids.py
+++ evaluation-arxiv-ids.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 from utils.azure_client import AzureClient
 from utils.citations import get_arxiv_abstract
-from utils.helpers import generate_relevance_evaluation_prompt, load_api_key
+from utils.helpers import generate_relevance_evaluation_prompt, load_api_key, generate_related_work_score_prompt
 
 load_dotenv()
 
@@ -15,43 +15,93 @@ prompting_client = AzureClient(
     api_key=load_api_key(os.getenv("KEY_LOCATION")),
 )
 
+OUTPUT_ARXIX_ID_ABSTRACT_RELEVANCE_SCORE_PATH = 'data/output_arxiv-ids.csv'
+OUTPUT_RELATED_WORK_RELEVANCE_SCORE_PATH = 'data/output_related_work_scores.csv'
+
 papers_df = pd.read_csv('data/papers.csv')
 arxiv_id_df = pd.read_csv('data/papers-arxiv-ids.csv')
 
-output_data = []
+# output_scores_data = []
+#
+# for i, row in arxiv_id_df.iterrows():
+#     abstract: str = papers_df['abstract'][i]
+#     arxiv_ids_source: list[str] = row['arxiv_ids_source'][1:-1].split(', ')
+#     source_scores = []
+#     for arxiv_id in arxiv_ids_source:
+#         abstract2: str = get_arxiv_abstract(arxiv_id)
+#         prompt: str = generate_relevance_evaluation_prompt(abstract, abstract2)
+#         response: str = prompting_client.get_completions(
+#             prompt,
+#             os.getenv("AZURE_PROMPTING_MODEL_VERSION")
+#         )
+#         rating: int = int(response)
+#         source_scores.append(rating)
+#
+#     gpt_scores = []
+#     arxiv_ids_gpt4o_mini: list[str] = row['arxiv_ids_gpt4o_mini'][1:-1].split(', ')
+#     for arxiv_id in arxiv_ids_gpt4o_mini:
+#         abstract2: str = get_arxiv_abstract(arxiv_id)
+#         prompt: str = generate_relevance_evaluation_prompt(abstract, abstract2)
+#         response: str = prompting_client.get_completions(
+#             prompt,
+#             os.getenv("AZURE_PROMPTING_MODEL_VERSION")
+#         )
+#         rating: int = int(response)
+#         gpt_scores.append(rating)
+#
+#     output_scores_data.append({
+#         'title': row['title'],
+#         'source_score': "|".join([str(i) for i in source_scores]),
+#         'source_length': len(source_scores),
+#         'gpt4o_score': "|".join([str(i) for i in gpt_scores]),
+#         'gpt4o_length': len(gpt_scores)
+#     })
+#
+#
+# pd.DataFrame(output_scores_data).to_csv(OUTPUT_ARXIX_ID_ABSTRACT_RELEVANCE_SCORE_PATH, index=False)
 
-for i, row in arxiv_id_df.iterrows():
-    abstract: str = papers_df['abstract'][i]
-    arxiv_ids_source: list[str] = row['arxiv_ids_source'][1:-1].split(', ')
-    source_scores = []
-    for arxiv_id in arxiv_ids_source:
-        abstract2: str = get_arxiv_abstract(arxiv_id)
-        prompt: str = generate_relevance_evaluation_prompt(abstract, abstract2)
-        response: str = prompting_client.get_completions(
-            prompt,
-            os.getenv("AZURE_PROMPTING_MODEL_VERSION")
-        )
-        rating: int = int(response)
-        source_scores.append(rating)
+output_df = pd.read_csv('data/output.csv')
 
-    gpt_scores = []
-    arxiv_ids_gpt4o_mini: list[str] = row['arxiv_ids_gpt4o_mini'][1:-1].split(', ')
-    for arxiv_id in arxiv_ids_gpt4o_mini:
-        abstract2: str = get_arxiv_abstract(arxiv_id)
-        prompt: str = generate_relevance_evaluation_prompt(abstract, abstract2)
-        response: str = prompting_client.get_completions(
-            prompt,
-            os.getenv("AZURE_PROMPTING_MODEL_VERSION")
-        )
-        rating: int = int(response)
-        gpt_scores.append(rating)
+output_related_work_scores_data = []
 
-    output_data.append({
+for i, row in papers_df.iterrows():
+    abstract = papers_df['abstract'][i]
+    source_related_work = papers_df['related_works'][i]
+    gpt4o_mini_related_work = papers_df['related_works_gpt4o_mini'][i]
+    our_related_work = output_df['related_works'][i]
+
+    prompt_source: str = generate_related_work_score_prompt(abstract, source_related_work)
+    prompt_gpt4o_mini: str = generate_related_work_score_prompt(abstract, gpt4o_mini_related_work)
+    prompt_our: str = generate_related_work_score_prompt(abstract, our_related_work)
+
+    result_source: str = prompting_client.get_completions(
+        prompt_source,
+        os.getenv("AZURE_PROMPTING_MODEL_VERSION")
+    )
+    rating_source: int = int(result_source)
+
+    result_gpt4o_mini: str = prompting_client.get_completions(
+        prompt_gpt4o_mini,
+        os.getenv("AZURE_PROMPTING_MODEL_VERSION")
+    )
+    rating_gpt4o_mini: int = int(result_gpt4o_mini)
+
+    result_our: str = prompting_client.get_completions(
+        prompt_our,
+        os.getenv("AZURE_PROMPTING_MODEL_VERSION")
+    )
+    rating_our: int = int(result_our)
+
+
+    output_related_work_scores_data.append({
         'title': row['title'],
-        'source_score': "|".join([str(i) for i in source_scores]),
-        'source_length': len(source_scores),
-        'gpt4o_score': "|".join([str(i) for i in gpt_scores]),
-        'gpt4o_length': len(gpt_scores)
+        'score_source': rating_source,
+        'score_gpt4o_mini': rating_gpt4o_mini,
+        'score_ours': rating_our
     })
 
-pd.DataFrame(output_data).to_csv('data/output_arxiv-ids.csv', index=False)
+pd.DataFrame(output_related_work_scores_data).to_csv(OUTPUT_RELATED_WORK_RELEVANCE_SCORE_PATH, index=False)
+
+
+
+

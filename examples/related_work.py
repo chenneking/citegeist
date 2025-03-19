130c00fbb04c3e27f44d83ffe04f45539c7b9fef
chenneking <39014772+chenneking@users.noreply.github.com>
2025-03-19 11:54:47 -0400

    fixed examples/related_work.py

--- examples/related_work.py
+++ examples/related_work.py
@@ -1,21 +1,28 @@
 from citegeist import generator
 
-if __name__ == '__main__':
-    gen = generator.Generator()
+if __name__ == "__main__":
+    # Setup Generator
+    gen = generator.Generator(
+        llm_provider="gemini",  # choice of: Azure (OpenAI Studio), Anthropic, Gemini, Mistral, and OpenAI
+    )
 
     # Define inputs
-    abstract: str = "Traditional methods for aligning Large Language Models (LLMs), such as Reinforcement Learning from Human Feedback (RLHF) and Direct Preference Optimization (DPO), rely on implicit principles, limiting interpretability. Constitutional AI (CAI) offers an explicit, rule-based framework for guiding LLM alignment. Building on this, we refine the Inverse Constitutional AI (ICAI) algorithm, which extracts constitutions from preference datasets. By improving principle generation, clustering, and embedding processes, our approach enhances the accuracy and generalizability of extracted principles across synthetic and real-world datasets. Our results highlight the potential of these principles to foster more transparent and adaptable alignment methods, offering a promising direction for future advancements beyond traditional fine-tuning."
+    abstract: str = """\
+    Traditional methods for aligning Large Language Models (LLMs), such as Reinforcement Learning from Human Feedback 
+    (RLHF) and Direct Preference Optimization (DPO), rely on implicit principles, limiting interpretability. 
+    Constitutional AI (CAI) offers an explicit, rule-based framework for guiding LLM alignment. Building on this, we 
+    refine the Inverse Constitutional AI (ICAI) algorithm, which extracts constitutions from preference datasets. By 
+    improving principle generation, clustering, and embedding processes, our approach enhances the accuracy and 
+    generalizability of extracted principles across synthetic and real-world datasets. Our results highlight the 
+    potential of these principles to foster more transparent and adaptable alignment methods, offering a promising 
+    direction for future advancements beyond traditional fine-tuning.
+    """
     breadth: int = 10
     depth: int = 2
     diversity: float = 0.0
 
     # Get result
-    result: dict = gen.generate_related_work(
-        abstract=abstract,
-        breadth=breadth,
-        depth=depth,
-        diversity=diversity
-    )
+    result: dict = gen.generate_related_work(abstract=abstract, breadth=breadth, depth=depth, diversity=diversity)
 
-    print(result['related_works'])
-    print(result['citations'])
+    print(result["related_works"])
+    print(result["citations"])

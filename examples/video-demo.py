30e9250882f4dfde9b2e425a3d9b7a56112333d3
chenneking <39014772+chenneking@users.noreply.github.com>
2025-03-28 15:27:07 -0400

    added video demo code

---
+++ examples/video-demo.py
@@ -0,0 +1,28 @@
+"""
+Video demonstration on how to use the citegeist pipeline to generate a related works section.
+"""
+
+import os
+from citegeist import Generator
+
+if __name__ == "__main__":
+    # Setup Generator
+    generator = Generator(
+        llm_provider="gemini",
+        api_key=os.environ.get("GEMINI_API_KEY"),
+        model_name="gemini-2.0-flash",
+        database_uri=os.environ.get("MILVUS_URI"),
+        database_token=os.environ.get("MILVUS_TOKEN"),
+    )
+
+    # Define inputs
+    abstract: str = "Traditional methods for aligning Large Language Models (LLMs), such as Reinforcement Learning from Human Feedback (RLHF) and Direct Preference Optimization (DPO), rely on implicit principles, limiting interpretability. Constitutional AI (CAI) offers an explicit, rule-based framework for guiding LLM alignment. Building on this, we refine the Inverse Constitutional AI (ICAI) algorithm, which extracts constitutions from preference datasets. By improving principle generation, clustering, and embedding processes, our approach enhances the accuracy and generalizability of extracted principles across synthetic and real-world datasets. Our results highlight the potential of these principles to foster more transparent and adaptable alignment methods, offering a promising direction for future advancements beyond traditional fine-tuning."
+    breadth: int = 10
+    depth: int = 2
+    diversity: float = 0.0
+
+    # Get result
+    result: dict = generator.generate_related_work(abstract=abstract, breadth=breadth, depth=depth, diversity=diversity)
+
+    print(f"Related Work:\n{result["related_works"]}")
+    print(f"Citations:\n{result["citations"]}")

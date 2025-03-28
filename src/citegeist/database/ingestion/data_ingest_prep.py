11fad8a8b7750523c172606501a1df5762f6201e
chenneking <39014772+chenneking@users.noreply.github.com>
2025-03-28 04:12:13 -0400

    renamed file

---
+++ src/citegeist/database/ingestion/data_ingest_prep.py
@@ -0,0 +1,54 @@
+import os.path
+import sys
+
+import jsonlines
+from pymilvus import DataType, MilvusClient
+from pymilvus.bulk_writer import BulkFileType, LocalBulkWriter
+
+"""
+Usage:
+python data_ingest_prep.py <path to source data (.jsonl)> <output file directory>
+"""
+# Input validation
+assert len(sys.argv) == 3
+assert os.path.isfile(sys.argv[1]) and os.path.isdir(sys.argv[2])
+assert sys.argv[1].endswith(".jsonl")
+
+# Initiate Milvus Client
+client = MilvusClient(
+    uri=os.environ["MILVUS_URI"], token=os.environ["MILVUS_TOKEN"]
+)  # TODO: Define client credentials here
+
+# Define Milvus DB Schema
+schema = client.create_schema(auto_id=False)
+schema.add_field(field_name="id", datatype=DataType.VARCHAR, max_length=50, is_primary=True)
+schema.add_field(field_name="embedding", datatype=DataType.FLOAT_VECTOR, dim=768)
+schema.add_field(field_name="hash", datatype=DataType.VARCHAR, max_length=64)
+schema.add_field(field_name="topic", datatype=DataType.INT64)
+
+# Add index on the embedding vector values
+index_params = MilvusClient.prepare_index_params()
+index_params.add_index(field_name="embedding", metric_type="COSINE", index_type="FLAT")
+
+# Create Milvus collection based on schema
+client.create_collection(collection_name="abstracts", schema=schema, index_params=index_params)
+
+# Initialize localbulkwriter
+writer = LocalBulkWriter(schema=schema, local_path=sys.argv[2], file_type=BulkFileType.PARQUET)
+
+# Read in the entire jsonl file data and insert it into the DB
+count = 0
+# Optionally, we can also filter to only include certain keys.
+# accepted_keys = [79, 77, 76, 63, 62, 60, 40, 30, 19, 16]
+with jsonlines.open(sys.argv[2]) as reader:
+    for obj in reader:
+        # if obj["topic"] not in accepted_keys:
+        #     continue
+        if count % 10000 == 0:
+            writer.commit()
+            print(f"Processed & committed {count} entries")
+        writer.append_row({"id": obj["id"], "embedding": obj["embedding"], "hash": obj["hash"], "topic": obj["topic"]})
+        count += 1
+
+print(f"Finished processing {count} entries")
+writer.commit()
